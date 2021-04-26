b93dbc0a93b5f95b26c7e3e38f135fed50d38f3f
idesignerse <heberandrade@gmail.com>
2021-04-26 10:24:05 +0200

    Starter Habits Website
    Using >DataBase
    Usng > Razor Pages

---
+++ Models/HabitContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Habit.Models
+{
+    public class HabitContext : DbContext
+    {
+        public DbSet<Habit> Habits { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            DbContextOptionsBuilder dbContextOptionsBuilder = optionsBuilder.UseSqlServer(
+            @"Data Source = S5D011\SQLEXPRESS; Initial Catalog = HabitsCatalogen;" +
+            " Integrated Security = True; Connect Timeout = 30; Encrypt = False;" +
+            " TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False"
+        );
+        }
+    }
+}
+//Saving to Github
