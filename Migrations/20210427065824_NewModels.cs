2151136ccd6a691d7c7abf61779a9164956e3cf5
idesignerse <heberandrade@gmail.com>
2021-04-27 09:05:53 +0200

    update

---
+++ Migrations/20210427065824_NewModels.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MVC_Habit.Migrations
+{
+    public partial class NewModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Program",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NameProgram = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TypeProgram = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DurationProgram = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Observation = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Program", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Program");
+        }
+    }
+}
